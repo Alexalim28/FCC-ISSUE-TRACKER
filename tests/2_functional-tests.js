e1cbe3c59d769ac3a164224e6f2e393683c3a17d
Alexalim28 <alexis.ahab@gmail.com>
2022-01-16 14:20:16 +0100

    Completed all routing features

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,33 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+suite("Functional Tests", function () {
+  suite("Testing POST /api/issues/apitest routes", function () {
+    test("POST request with every fields", function (done) {
+      chai
+        .request(server)
+        .post("/api/issues/apitest")
+        .send({
+          issue_title: "A new issue",
+          issue_text: "Some description",
+          created_by: "Salim",
+          assigned_to: "Alex",
+          status_text: "in Qa",
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.issue_title, "A new issue");
+          assert.equal(res.body.issue_text, "Some description");
+          assert.equal(res.body.created_by, "Salim");
+          assert.equal(res.body.assigned_to, "Alex");
+          assert.equal(res.body.status_text, "in Qa");
+          done();
+        });
+    });
+  });
 });
